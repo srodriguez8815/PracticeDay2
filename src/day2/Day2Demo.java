cce1ccb2faf418a989847af3478468bc8af9381a
96sam <96sam@DESKTOP-U2TUVL3.mynetworksettings.com>
2025-07-31 15:12:21 -0400

    implemented getinfo()

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -2,8 +2,8 @@ package day2;
 
 public class Day2Demo {
 
-	
-	public static void main (String args []) {
-		System.out.println("Day 2 gift");
+	public void getInfo() {
+		System.out.println("Pushing code");
+		
 	}
 }
